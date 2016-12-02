e5448c39447a491b9d2b92ef4b622e1ef5e591aa
Josh <josh.yee@tafensw.net.au>
2016-12-02 12:01:17 +1100

    Initial Commit

---
+++ Laser Sailing/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Laser Sailing
+//
+//  Created by Josh Yee on 2/12/2016.
+//  Copyright © 2016 Performance Sailcraft Australia. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
