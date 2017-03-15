1ddc6e5895836696aea20f761f1fa6811e38b923
Josh <josh.yee@tafensw.net.au>
2017-03-15 11:59:30 +1100

    15/03/17

---
+++ Laser Sailing/ChatViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewController.swift
+//  Laser Sailing
+//
+//  Created by Josh Yee on 24/2/17.
+//  Copyright © 2017 Performance Sailcraft Australia. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class Chat: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
